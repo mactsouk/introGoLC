d2624ffffbc45988ef94164c7154cf8c74fc6e65
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-09-03 10:14:28 +0300

    Added error type example

---
+++ day1/s2/errorType.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+    "log"
+)
+
+func division(x, y int) (int, error) {
+    if y == 0 {
+        return 0, errors.New("Cannot divide by zero!")
+    }
+    return x / y, nil
+}
+
+func main() {
+    result, err := division(2, 2)
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Println("The result is", result)
+
+    result2, err := division(2, 0)
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Println("The result is", result2)
+}
+
