b49384d47d5b80c14f178f5c51643a52216a7668
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-09-03 10:19:12 +0300

    Added more files

---
+++ day1/s2/aPackage.go
@@ -0,0 +1,18 @@
+package aPackage
+
+import (
+    "fmt"
+)
+
+func A() {
+    fmt.Println("This is function A!")
+}
+
+func B() {
+    fmt.Println("privateConstant:", privateConstant)
+}
+
+const MyConstant = 123
+const privateConstant = 21
+
+
---
+++ day1/s2/loops.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    for i := 0; i < 100; i++ {
+        if i%20 == 0 {
+            continue
+        }
+        if i == 95 {
+            break
+        }
+        fmt.Print(i, " ")
+    }
+
+    fmt.Println()
+    i := 10
+    for {
+        if i < 0 {
+            break
+        }
+        fmt.Print(i, " ")
+        i--
+    }
+
+    fmt.Println()
+    i = 0
+    anExpr := true
+    for ok := true; ok; ok = anExpr {
+        if i > 10 {
+            anExpr = false
+        }
+
+        fmt.Print(i, " ")
+        i++
+    }
+    fmt.Println()
+
+    anArray := [5]int{0, 1, -1, 2, -2}
+    for i, v := range anArray {
+        fmt.Println("ind:", i, "v: ", v)
+    }
+}
