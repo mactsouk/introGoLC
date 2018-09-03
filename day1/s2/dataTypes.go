3427b5df371f8350ad48e62791ded9bc67da4d15
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-09-03 10:10:28 +0300

    Added dataTypes.go

---
+++ day1/s2/dataTypes.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+    var m, n float64
+    var c complex128
+    x := 12
+
+    fmt.Println(x)
+    fmt.Printf("Type of c: %T\n", c)
+    fmt.Println("m, n:", m, n)
+}
