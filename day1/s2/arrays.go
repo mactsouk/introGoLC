3e24e627acd7f0d1dedd78e9ec19af8ed7760591
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-09-15 22:21:31 +0300

    Another way to create an array

--- day1/s2/arrays.go
+++ day1/s2/arrays.go
@@ -5,32 +5,35 @@ import (
 )
 
 func main() {
-    // myArray := []int{1, 2, 4, -4}
     myArray := [4]int{1, 2, 4, -4}
+    // Or use the following:
+    // myArray := []int{1, 2, 4, -4}
+
     length := len(myArray)
+
     for i := 0; i < length; i++ {
         fmt.Printf("%d ", myArray[i])
     }
     fmt.Printf("\n")
 
     otherArray := [...]int{-2, 6, 7, 8}
-
     for _, number := range otherArray {
         fmt.Printf("%d ", number)
     }
     fmt.Printf("\n")
+
     twoD := [3][3]int{
                {1, 2, 3},
                {4, 5, 6},
                {7, 8, 9}}
 
     twoD[1][2] = 15
-
     for _, number := range twoD {
         for _, other := range number {
             fmt.Printf("%d ", other)
         }
     }
+
     fmt.Printf("\n")
 }
 
