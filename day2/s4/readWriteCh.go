b7309699af45fe95d11038ff8044c316d31f17c7
Mihalis Tsoukalos <mihalistsoukalos@gmail.com>
2018-09-15 11:02:37 +0300

    Closed channels

--- day2/s4/readWriteCh.go
+++ day2/s4/readWriteCh.go
@@ -7,22 +7,33 @@ import (
 
 func writeChannel(c chan<- int, x int) {
     fmt.Println(x)
+    defer close(c)
     c <- x
-    close(c)
     fmt.Println(x)
 }
 
 func main() {
     c := make(chan int)
+
     go writeChannel(c, 10)
     time.Sleep(2 * time.Second)
+
     fmt.Println("Read:", <-c)
     time.Sleep(2 * time.Second)
+
     _, ok := <-c
     if ok {
         fmt.Println("Channel is open!")
     } else {
         fmt.Println("Channel is closed!")
     }
+
+    _, ok = <-c
+    if ok {
+        fmt.Println("Channel is open!")
+    } else {
+        fmt.Println("Channel is closed!")
+    }
+
 }
 
